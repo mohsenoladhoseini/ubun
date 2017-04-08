8fad1fd63f8f9bbfaac080d6ad050ea9c3a79ede
Moein Alinaghian <git@nixoeen.com>
2017-04-08 22:32:12 +0200

    Initial commit

---
+++ footer_body.php
@@ -0,0 +1,58 @@
+	</div>
+</div>
+<div id="ubuntuir_footer">
+	<a id="ubuntuir_logo_footer" href="http://ubuntu.ir/"><img src="https://static.ubuntu.ir/images/logo_footer.png" /></a>
+	<div id="ubuntuir_footer_navbar">
+		<ul>
+			<li id="ubuntuir_footer_navbar_first">
+				<div id="ubuntuir_footer_navbar_inner">
+					<ul>
+						<li id="ubuntuir_footer_navbar_inner_header"><a href="http://ubuntu.ir">اوبونتو</a></li>
+						<li><a href="http://forum.ubuntu.ir/index.php/board,38.0.html">اخبار</a></li>
+						<li><a href="http://ubuntu.com/download">دریافت</a></li>
+					</ul>
+				</div>
+			</li>
+			<li>
+				<div id="ubuntuir_footer_navbar_inner">
+					<ul>
+						<li id="ubuntuir_footer_navbar_inner_header"><a href="http://forum.ubuntu.ir">انجمن‌های پشتیبانی</a></li>
+						<li><a href="http://forum.ubuntu.ir/index.php/topic,242.0.html">قوانین انجمن</a></li>
+						<li><a href="http://forum.ubuntu.ir/index.php?action=register">ثبت‌نام</a></li>
+					</ul>
+				</div>
+			</li>
+			<li id="ubuntuir_footer_navbar_last">
+				<div id="ubuntuir_footer_navbar_inner">
+					<ul>
+						<li id="ubuntuir_footer_navbar_inner_header"><a href="http://ubuntu.ir/support">کمک و پشتیبانی</a></li>
+						<li><a href="https://wiki.ubuntu.ir">ویکی فارسی</a></li>
+						<li><a href="http://ubuntu.ir/irc">کانال IRC</a></li>
+					</ul>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<div id="ubuntuir_footer_text">
+		© ۱۳۹۶ جامعه پارسی‌زبان اوبونتو<br />
+		© ۱۳۹۶ شرکت کنونیکال.<br />
+		 اوبونتو و کنونیکال نام‌های تجاری متعلق به Canonical Ltd هستند.
+	</div>
+	<br /><br />
+</div>
+<!-- Stats -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["setDomains", ["*.ubuntu.ir","*.forum.ubuntu.ir","*.paste.ubuntu.ir","*.wiki.ubuntu.ir"]]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//anormeda.com/stats/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 7]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//anormeda.com/stats/piwik.php?idsite=7" style="border:0;" alt="" /></p></noscript>
+<!-- /Stats -->
---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html dir="rtl">
+<head>
+	<title>Iranian Ubuntu Community</title>
+	<meta charset="utf-8" />
+	<meta name="description" content="Iranian Ubuntu Community" />
+	<meta name="keywords" content="ubuntu, gnu, linux, iran, persian, parsi, farsi, اوبونتو, گنو, لینوکس, فارسی, پارسی, ایران" />
+	<meta name="author" content="Moein Alinaghian" />
+	<link rel="shortcut icon" href="https://static.ubuntu.ir/images/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="https://static.ubuntu.ir/css/ubuntuir.css" />
+	<? if (isset($tpl["css"])) { ?><link rel="stylesheet" type="text/css" href="<?=$tpl["css"]?>" /><? }; ?>
+</head>
+<body>
